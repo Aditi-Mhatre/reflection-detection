33e8f9a040ded3adadbb32b5f0eb883a90215dde
Aditi Mhatre <am.doc24@gmail.com>
2024-11-29 11:14:21 +0100

    Added Traditional Methods

--- Traditional Methods/adaptive_thresholding.py
+++ Traditional Methods/adaptive_thresholding.py
@@ -30,12 +30,6 @@ cv2.imwrite('inspection/' + img_name + '_h.png',threshInv)
 cv2.waitKey(0)
 
 
-# apply Otsu's automatic thresholding
-""" (T, threshInv) = cv2.threshold(blurred, 0, 255,
-	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
-cv2.imshow("Otsu Thresholding", threshInv)
-cv2.waitKey(0) """
-
 # instead of manually specifying the threshold value, we can use
 # adaptive thresholding to examine neighborhoods of pixels and
 # adaptively threshold each neighborhood
